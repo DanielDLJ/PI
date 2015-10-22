caa35e11946ac1fd6103b97e5f6ada46bb7cf9b8
UTFPR <UTFPR@192.168.16.235>
2015-10-22 10:56:16 -0200

    1 Vez

---
+++ src/partes/Partes.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partes;
+
+/**
+ *
+ * @author UTFPR
+ */
+public interface Partes {
+    public void ligar();
+    public void desligar();
+    public void funcionar();
+}
